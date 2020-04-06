e7836f44ff3a7212e6ea9c7a1628a579e6918350
Valentin Klinghammer <valentin@prototype.berlin>
2020-04-06 15:03:27 +0200

    WIP

---
+++ android/src/main/java/berlin/prototype/capacitormsal/Msal.java
@@ -0,0 +1,55 @@
+package berlin.prototype.capacitormsal;
+
+import android.Manifest;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.microsoft.identity.client.*;
+
+@NativePlugin(
+        permissions={
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_NETWORK_STATE
+        }
+)
+public class Msal extends Plugin {
+
+    @PluginMethod()
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+
+    @PluginMethod()
+    public void getAccessToken(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+
+    @PluginMethod()
+    public void getAccessTokenSilently(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+
+    @PluginMethod()
+    public void signOut(PluginCall call) {
+        String value = call.getString("value");
+
+        JSObject ret = new JSObject();
+        ret.put("value", value);
+        call.success(ret);
+    }
+}
